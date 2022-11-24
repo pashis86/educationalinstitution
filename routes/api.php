09908b9ea988df609a93b104d3adc91d1841645b
pashis86 <sarunas@europemail.com>
2022-11-24 09:33:16 +0200

    commit2

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ use App\Http\Controllers\UsersController as Users;
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
-// });
+
 
 Route::post('/login', [Users::class, 'login']);
 Route::get('/logout', [Users::class, 'logout'])->middleware('auth:api');
